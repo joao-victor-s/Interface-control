9d487347bec0b1823331bdb67cebc9cb50850144
Mateus Goto <m241559@dac.unicamp.br>
2022-06-20 11:37:11 -0300

    [ADDED] refined outputs and more setters

--- NHR9400/NRH9400.py
+++ NHR9400/NRH9400.py
@@ -5,27 +5,31 @@ from Utility import IPFinder
 class NHR9400:
     
     def __init__(self, name):
-        self.__id = random.randrange(100, 300, 2)
         self.__name = name
         self.__s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.__s.timeout(1)
+        pass
 
     
 
     def setIp(self):
+        pass
+    def getIp(self):
         pass
     #Function to see if exist any error in the carry
     def checkErrors(self):
         self.__s.send("SYSTem:ERRor?")
     #set limit voltage of all channels
     def setVoltage(self,voltage):
-        self.__s.send("VOLT " + voltage + "\n")
+        self.__s.send("SOUR:VOLT " + voltage + "\n")
     
     def setCurrent(self, current):
-        self.__s.send("CURR " + current + "\n")
-    def setPower():
-    
-    def setFreq():
+        self.__s.send("SOUR:CURR " + current + "\n")
+
+    def setPower(self, pow):
+        self.__s.send("SOUR:POW " + pow + "\n")
+    def setFreq(self, freq):
+        self.__s.send("SOUR:POW " + freq + "\n")
 
     def getVoltage(self):
         value = self.__s.send("FETCh:VOLTage?")
---
+++ Utility/refineOutput.py
@@ -0,0 +1,10 @@
+class refineOutput:
+
+    def byteToFloat(recv):
+        recv = recv.decode("UTF-8")
+        recv = recv.rstrip("\n\x00")
+        return float(recv)
+
+    def byteToString(recv):
+        recv = recv.decode("UTF-8")
+        return recv
